7c31ccd607e0a2c3fc11a6312cec285a30562190
Franek <poczta.f@protonmail.com>
2021-04-14 10:52:50 +0200

    feat: Create layout for portion weight screen

--- app/src/main/java/link/epoczta/carbscalc/ui/portionweight/PortionWeightFragment.kt
+++ app/src/main/java/link/epoczta/carbscalc/ui/portionweight/PortionWeightFragment.kt
@@ -9,23 +9,27 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import link.epoczta.carbscalc.R
+import link.epoczta.carbscalc.databinding.FragmentPortionCarbsBinding
+import link.epoczta.carbscalc.databinding.FragmentPortionWeightBinding
+import link.epoczta.carbscalc.ui.portioncarbs.PortionCarbsViewModel
 
 class PortionWeightFragment : Fragment() {
 
+    private lateinit var binding: FragmentPortionWeightBinding
     private lateinit var viewModel: PortionWeightViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
-    ): View? {
-        viewModel =
-                ViewModelProvider(this).get(PortionWeightViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_portion_weight, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        viewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+    ): View {
+
+        binding = FragmentPortionWeightBinding.inflate(inflater)
+        viewModel = ViewModelProvider(this).get(PortionWeightViewModel::class.java)
+
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        return binding.root
     }
 }
