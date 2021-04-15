1315c49a78f1510baf1993107afe425b14c89e58
Franek <poczta.f@protonmail.com>
2021-04-15 12:21:54 +0200

    refactor: Change the way calculations are triggered

--- app/src/main/java/link/epoczta/carbscalc/ui/portioncarbs/PortionCarbsFragment.kt
+++ app/src/main/java/link/epoczta/carbscalc/ui/portioncarbs/PortionCarbsFragment.kt
@@ -1,9 +1,12 @@
 package link.epoczta.carbscalc.ui.portioncarbs
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
@@ -11,7 +14,7 @@ import link.epoczta.carbscalc.MainViewModel
 import link.epoczta.carbscalc.R
 import link.epoczta.carbscalc.databinding.FragmentPortionCarbsBinding
 
-class PortionCarbsFragment : Fragment() {
+class PortionCarbsFragment : Fragment(), TextWatcher {
 
     private lateinit var binding: FragmentPortionCarbsBinding
     private val viewModel: MainViewModel by activityViewModels()
@@ -27,14 +30,20 @@ class PortionCarbsFragment : Fragment() {
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
 
-        viewModel.portionWeightString.observe(viewLifecycleOwner) {
-            viewModel.calculatePortionCarbs()
-        }
-
-        viewModel.carbsIn100gString.observe(viewLifecycleOwner) {
-            viewModel.calculatePortionCarbs()
-        }
+        binding.portionWeightEdit.addTextChangedListener(this)
+        binding.carbsIn100gEdit.addTextChangedListener(this)
 
         return binding.root
     }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+    }
+
+    override fun afterTextChanged(s: Editable?) {
+        viewModel.calculatePortionCarbs()
+    }
+
 }
--- app/src/main/java/link/epoczta/carbscalc/ui/portionweight/PortionWeightFragment.kt
+++ app/src/main/java/link/epoczta/carbscalc/ui/portionweight/PortionWeightFragment.kt
@@ -1,6 +1,8 @@
 package link.epoczta.carbscalc.ui.portionweight
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,15 +11,15 @@ import androidx.fragment.app.activityViewModels
 import link.epoczta.carbscalc.MainViewModel
 import link.epoczta.carbscalc.databinding.FragmentPortionWeightBinding
 
-class PortionWeightFragment : Fragment() {
+class PortionWeightFragment : Fragment(), TextWatcher {
 
     private lateinit var binding: FragmentPortionWeightBinding
     private val viewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
 
         binding = FragmentPortionWeightBinding.inflate(inflater)
@@ -25,14 +27,19 @@ class PortionWeightFragment : Fragment() {
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
 
-        viewModel.carbsInPortionString.observe(viewLifecycleOwner) {
-            viewModel.calculatePortionWeight()
-        }
-
-        viewModel.carbsIn100gString.observe(viewLifecycleOwner) {
-            viewModel.calculatePortionWeight()
-        }
+        binding.carbsInPortionEdit.addTextChangedListener(this)
+        binding.carbsIn100gEdit.addTextChangedListener(this)
 
         return binding.root
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+    }
+
+    override fun afterTextChanged(s: Editable?) {
+        viewModel.calculatePortionWeight()
     }
 }
