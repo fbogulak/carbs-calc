ddb6fd08f42486f687552fce14bc0f93e2b0941a
Franek <poczta.f@protonmail.com>
2021-04-16 16:33:39 +0200

    feat: Insert zero before decimal separator

--- app/src/main/java/link/epoczta/carbscalc/utils/DecimalDigitsInputFilter.kt
+++ app/src/main/java/link/epoczta/carbscalc/utils/DecimalDigitsInputFilter.kt
@@ -2,36 +2,38 @@ package link.epoczta.carbscalc.utils
 
 import android.text.InputFilter
 import android.text.Spanned
-import java.text.DecimalFormat
 import java.text.DecimalFormatSymbols
-import java.util.*
 import java.util.regex.Pattern
 
 class DecimalDigitsInputFilter(digitsBeforeSeparator: Int, digitsAfterSeparator: Int) :
     InputFilter {
 
     companion object {
-        private const val SEPARATORS = "[\\.\\,]"
+        private const val SEPARATORS = "[.,]"
     }
 
     private val pattern: Pattern
+    private val separatorPattern: Pattern
 
     init {
-        val b = "(-?\\d{1,$digitsBeforeSeparator})"
+        val b = "(\\d{1,$digitsBeforeSeparator})"
         val a = "(\\d{1,$digitsAfterSeparator})"
         val s = SEPARATORS
 
         val numberRegex = StringBuilder()
-            .append("(-)")
+            .append("($s)")
             .append("|")
             .append("($b$s$a)")
             .append("|")
             .append("($b$s)")
             .append("|")
             .append("($b)")
+            .append("|")
+            .append("($s$a)")
             .toString()
 
         pattern = Pattern.compile(numberRegex)
+        separatorPattern = Pattern.compile(s)
     }
 
 
@@ -50,11 +52,17 @@ class DecimalDigitsInputFilter(digitsBeforeSeparator: Int, digitsAfterSeparator:
 
         val matcher = pattern.matcher(input)
 
-        return if (!matcher.matches()) {
+        val separatorMatcher = separatorPattern.matcher(input.firstOrNull()?.toString() ?: "")
+        val isSeparatorFirst = separatorMatcher.matches()
+
+        val localSeparator = DecimalFormatSymbols.getInstance().decimalSeparator.toString()
+        return if (!matcher.matches())
             ""
-        } else {
-            val localSeparator = DecimalFormatSymbols.getInstance().decimalSeparator.toString()
+        else if (isSeparatorFirst && source.isEmpty())
+            "0"
+        else if (isSeparatorFirst)
+            "0$localSeparator"
+        else
             source.replace(Regex("[.,]"), localSeparator)
-        }
     }
 }
