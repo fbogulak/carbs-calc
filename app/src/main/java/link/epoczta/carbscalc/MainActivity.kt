e831151e14c1a31fbc4ae4c38b0f7a2ed1cd41d4
Franek <poczta.f@protonmail.com>
2021-05-06 12:31:28 +0200

    feat: Set carbohydrates unit on first app launch
    When app is first launched, if default locale is pl_PL set unit
    to carb units, otherwise set it to grams.

--- app/src/main/java/link/epoczta/carbscalc/MainActivity.kt
+++ app/src/main/java/link/epoczta/carbscalc/MainActivity.kt
@@ -3,6 +3,7 @@ package link.epoczta.carbscalc
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
@@ -10,9 +11,12 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.preference.PreferenceManager
 import com.google.android.material.tabs.TabLayoutMediator
 import link.epoczta.carbscalc.databinding.ActivityMainBinding
 import link.epoczta.carbscalc.ui.adapters.ViewPagerFragmentAdapter
+import link.epoczta.carbscalc.utils.CarbUnits
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,6 +30,15 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         NavigationUI.setupActionBarWithNavController(this, navController)
+
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        if (sharedPreferences.getString("carbohydrates_unit", "") == "")
+            sharedPreferences.edit {
+                if (Locale.getDefault().equals(Locale("pl", "PL")))
+                    putString("carbohydrates_unit", getString(R.string.carbohydrate_units_value))
+                else
+                    putString("carbohydrates_unit", getString(R.string.grams_value))
+            }
     }
 
     override fun onSupportNavigateUp(): Boolean {
